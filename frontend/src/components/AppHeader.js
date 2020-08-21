84252e2524c4d802089cc4ec01d246110b26ffac
Youngchan <michael_cho77@naver.com>
2020-08-22 00:54:15 +0900

    Routing
    .

---
+++ frontend/src/components/AppHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AppHeader.scss";
+
+function AppHeader() {
+    return (
+        <div className="AppHeader">
+            Header
+        </div>
+    );
+}
+
+export default AppHeader;
